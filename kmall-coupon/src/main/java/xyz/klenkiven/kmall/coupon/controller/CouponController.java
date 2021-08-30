29fef103d0a139ec2ea5df2e12e59993472cfbeb
KlenKiven <wzl709@outlook.com>
2021-08-30 20:42:31 +0800

    test: Nacos 配置中心配置

--- kmall-coupon/src/main/java/xyz/klenkiven/kmall/coupon/controller/CouponController.java
+++ kmall-coupon/src/main/java/xyz/klenkiven/kmall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,6 @@ import xyz.klenkiven.kmall.coupon.service.CouponService;
 import xyz.klenkiven.common.utils.PageUtils;
 import xyz.klenkiven.common.utils.R;
 
-
-
 /**
  * 优惠券信息
  *
@@ -24,12 +24,29 @@ import xyz.klenkiven.common.utils.R;
  * @email wzl709@outlook.com
  * @date 2021-08-29 20:46:13
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
 
+    @Value("${coupon.demo.user.name}")
+    private String demoUserName;
+    @Value("${coupon.demo.user.age}")
+    private Integer demoUserAge;
+
+    /**
+     * Nacos配置中心测试
+     */
+    @RequestMapping("/config/test")
+    public R configTest() {
+        return R.ok().put("name", demoUserName).put("age", demoUserAge);
+    }
+
+    /**
+     * 远程调用测试
+     */
     @RequestMapping("/member/list")
     public R memberCoupons() {
         CouponEntity couponEntity = new CouponEntity();
