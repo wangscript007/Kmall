774b58f3a78ffeb23c37ae1d24ca44edb6fedc7d
KlenKiven <wzl709@outlook.com>
2021-09-01 10:33:46 +0800

    add: 配置网关路由，重写规则

--- kmall-admin/src/main/java/io/renren/KmallAdminApplication.java
+++ kmall-admin/src/main/java/io/renren/KmallAdminApplication.java
@@ -2,9 +2,14 @@ package io.renren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+/**
+ * 酷商城-后台管理系统
+ * @author klenkiven
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class KmallAdminApplication {
 
 	public static void main(String[] args) {
