f3a9226f296759c940a683648777c8e16b9fcbb0
2019006410-王子龙 <wzl709@outlook.com>
2021-10-06 20:30:51 +0800

    add: kmall-third-party OSS

---
+++ kmall-third-party/src/main/java/xyz/klenkiven/kmall/thirdparty/controller/OssController.java
@@ -0,0 +1,89 @@
+package xyz.klenkiven.kmall.thirdparty.controller;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * OSS Service Controller
+ * <p>This controller is used RPC
+ * </p>
+ * @author klenkiven
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/third-party/oss")
+@RequiredArgsConstructor
+public class OssController {
+
+    private final OSS ossClient;
+
+    /**
+     * Alibaba Cloud Access Key
+     */
+    @Value("${alibaba.cloud.access-key}")
+    private String accessId;
+
+    /**
+     * Alibaba Cloud OSS Service
+     */
+    @Value("${alibaba.cloud.oss.endpoint}")
+    private String endpoint;
+    @Value("${alibaba.cloud.oss.bucket}")
+    private String bucket;
+
+    /**
+     * Get OSS policy to upload file to OSS Bucket
+     */
+    @GetMapping("/policy")
+    public Map<String, String> policy() {
+        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
+        // callbackUrl为上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
+        // String callbackUrl = "http://88.88.88.88:8888";
+        String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dir = format + "/"; // 用户上传文件时指定的前缀。
+
+        Map<String, String> respMap = new LinkedHashMap<>();
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            // PostObject请求最大可支持的文件大小为5 GB，即CONTENT_LENGTH_RANGE为5*1024*1024*1024。
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes(StandardCharsets.UTF_8);
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+            // respMap.put("expire", formatISO8601Date(expiration));
+        } catch (Exception e) {
+            // Assert.fail(e.getMessage());
+            System.out.println(e.getMessage());
+        }
+
+        return respMap;
+    }
+
+}
