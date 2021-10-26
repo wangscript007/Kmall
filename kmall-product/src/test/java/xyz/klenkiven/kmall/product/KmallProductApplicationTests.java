15aa255401fb4ff0411d8dec6d45354aa0ca4115
2019006410-王子龙 <wzl709@outlook.com>
2021-10-26 15:42:16 +0800

    ADD: kmall-product Redis Data Integrated

--- kmall-product/src/test/java/xyz/klenkiven/kmall/product/KmallProductApplicationTests.java
+++ kmall-product/src/test/java/xyz/klenkiven/kmall/product/KmallProductApplicationTests.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import xyz.klenkiven.kmall.product.service.BrandService;
 
 import java.util.Arrays;
+import java.util.UUID;
 
 @SpringBootTest
 class KmallProductApplicationTests {
@@ -17,9 +20,18 @@ class KmallProductApplicationTests {
     @Autowired
     ApplicationContext applicationContext;
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
     @Test
     void contextLoads() {
         System.out.println(Arrays.toString(applicationContext.getBeanDefinitionNames()));
     }
 
+    @Test
+    void testStringRedis() {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set("hello", "world_" + UUID.randomUUID());
+        System.out.println("Saved Value: " + ops.get("hello"));
+    }
 }
