33ea07bc0ffd151d429e4a1793e2719c7fd4ce3e
王子龙 <wzl709@outlook.com>
2021-12-19 18:51:56 +0800

    add: kmall-auth-server Page Mapping and Code Sending Page Counter

---
+++ kmall-auth-server/src/main/java/xyz/klenkiven/auth/config/AuthWebConfigure.java
@@ -0,0 +1,32 @@
+package xyz.klenkiven.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Web MVC configurer for mapping request and pages
+ * @author klenkiven
+ */
+@Configuration
+public class AuthWebConfigure implements WebMvcConfigurer {
+
+    /**
+     * Registry for View and Controller Pages
+     * <p>
+     *     <code>
+     *          {@code @GetMapping("/login.html")}
+     *          public String loginPage() {
+     *              return "login";
+     *          }
+     *     </code>
+     * </p>
+     * @param registry View Controller Registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/reg.html").setViewName("reg");
+    }
+
+}
